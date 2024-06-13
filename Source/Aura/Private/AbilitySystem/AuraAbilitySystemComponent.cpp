5740a977c9a716c0c528d88d4427255ae60c48c3
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2024-06-13 22:51:08 +0100

    feat: [AURA-50] broadcast tags to widget overlay controller

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -14,10 +14,5 @@ void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Ability
 	FGameplayTagContainer TagContainer;
 	GameplayEffectSpec.GetAllAssetTags(TagContainer);
 
-	for (const FGameplayTag& Tag : TagContainer)
-	{
-		//TODO: Broadcast the tag to the widget controller
-		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
-		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Black, Msg);
-	}
+	EffectAssetTags.Broadcast(TagContainer);
 }
