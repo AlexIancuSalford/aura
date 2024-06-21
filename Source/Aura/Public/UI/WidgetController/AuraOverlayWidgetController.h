612a27610c8d2ec3ed3d011dbfb262f162affcf5
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2024-06-21 20:51:52 +0100

    feat: [AURA-56] refactor delegates

--- Source/Aura/Public/UI/WidgetController/AuraOverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraOverlayWidgetController.h
@@ -57,11 +57,6 @@ public:
 	FMessageWidgetRowSignature MessageWidgetRowDelegate;
 
 protected:
-	void HealthChanged(const FOnAttributeChangeData& Data) const;
-	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
-	void ManaChanged(const FOnAttributeChangeData& Data) const;
-	void MaxManaChanged(const FOnAttributeChangeData& Data) const;
-
 	template<typename T>
 	T* GetDatatableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
 
