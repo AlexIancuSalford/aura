60fc27118b96176723090fea8295950770d29e5c
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2024-07-11 23:17:02 +0100

    feat: [AURA-86] create atribute menu widget controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright AI
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+}
