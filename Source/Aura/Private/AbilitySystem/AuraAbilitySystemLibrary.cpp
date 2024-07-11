fae9181947637b30e5de2863579dd2faf0508a3c
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2024-07-11 23:40:32 +0100

    feat: [AURA-87] add ability system library to get overlay controller

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,26 @@
+// Copyright AI
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "UI/HUD/AuraHUD.h"
+
+UAuraOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AAuraHUD* hud = Cast<AAuraHUD>(PlayerController->GetHUD()))
+		{
+			AAuraPlayerState* PlayerState = PlayerController->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* AbilitySystemComponent = PlayerState->GetAbilitySystemComponent();
+			UAttributeSet* AttributeSet = PlayerState->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PlayerController, PlayerState, AbilitySystemComponent, AttributeSet);
+			return hud->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+
+	return nullptr;
+}
