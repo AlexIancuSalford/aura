516c8d028143502d43d0d969a5134203024ff937
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2024-07-12 17:51:58 +0100

    feat: [AURA-89] broadcast attribute info to widgets

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -1,7 +1,10 @@
 // Copyright AI
 
-
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
@@ -11,4 +14,12 @@ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
 	Super::BroadcastInitialValues();
+
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	check(AttributeInfo);
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AS->GetStrength();
+
+	AttributeInfoDelegate.Broadcast(Info);
 }
--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -48,6 +48,7 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void SetWidgetControllerParams(const FWidgetControllerParams& WcParams);
 
+	UFUNCTION(BlueprintCallable)
 	virtual void BroadcastInitialValues();
 	virtual void BindCallbacksToDependencies();
 
