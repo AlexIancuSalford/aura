d8e5d5b89c2fcc4a3bd009da22adee15d024ff47
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2024-08-20 00:06:29 +0100

    feat: [AURA-148] add damage exec calc class

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExectCalc_Damage.cpp
@@ -0,0 +1,22 @@
+// Copyright AI
+
+
+#include "AbilitySystem/ExecCalc/ExectCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UExectCalc_Damage::UExectCalc_Damage()
+{
+}
+
+void UExectCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+}
