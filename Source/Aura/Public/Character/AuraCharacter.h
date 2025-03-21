747ab26c184d64086e26687617888e8e0ab45150
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2025-03-21 19:12:02 +0000

    feat: [AURA-283] show xp to hud

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -1,16 +1,17 @@
-// Copywright AI
+// Copyright AI
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
+#include "Interaction/PlayerInterface.h"
 #include "AuraCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraCharacter : public AAuraCharacterBase
+class AURA_API AAuraCharacter : public AAuraCharacterBase, public IPlayerInterface
 {
 	GENERATED_BODY()
 
@@ -19,6 +20,7 @@ public:
 
 	virtual void PossessedBy(AController* NewController) override;
 	virtual void OnRep_PlayerState() override;
+	virtual void AddToXP_Implementation(int32 InXP) override;
 
 	FORCEINLINE virtual int32 GetPlayerLevel() override;
 
