24a9f144bf7a4ff08335f91ceacbeb80846f88c4
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2025-07-11 22:26:28 +0100

    feat: [AURA-329] add tags for debuff properties

--- Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
@@ -30,4 +30,16 @@ protected:
 	FGameplayTag DamageType;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
 	FScalableFloat Damage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	float DebuffChance = 20.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	float DebuffDamage = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	float DebuffFrequency = 1.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	float DebuffDuration = 5.f;
 };
