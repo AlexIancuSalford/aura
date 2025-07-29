07e5ddb120ce481a6568ed7a4dbca714eef9b415
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2025-07-30 00:32:38 +0100

    feat: [AURA-350] add gameplay cue actor for electrocute beam

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -205,3 +205,8 @@ ECharacterClass AAuraCharacterBase::GetCharacterClass_Implementation()
 {
 	return CharacterClass;
 }
+
+USkeletalMeshComponent* AAuraCharacterBase::GetWeapon_Implementation()
+{
+	return Weapon;
+}
