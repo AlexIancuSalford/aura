8cc27ecf2c0e7fc8a9a10c46a3102cc872425380
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2025-11-14 00:21:45 +0000

    feat: [AURA-358] set the number of additional target to ability level

--- Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -84,8 +84,7 @@ void UAuraBeamSpell::StoreAdditionalTargets(TArray<AActor*>& OutAdditionalTarget
 		MouseHitActor->GetActorLocation()
 		);
 
-	//int32 NumAdditionalTargets = FMath::Min(GetAbilityLevel() - 1, MaxNumShockTargets);
-	int32 NumAdditionalTargets = 5;
+	int32 NumAdditionalTargets = FMath::Min(GetAbilityLevel() - 1, MaxNumShockTargets);
 
 	UAuraAbilitySystemLibrary::GetClosestTargets(NumAdditionalTargets, OverlappingActors, OutAdditionalTargets, MouseHitActor->GetActorLocation());
 	
