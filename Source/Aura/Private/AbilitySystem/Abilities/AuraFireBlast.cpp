c216b594e41b6d157491d34b673f141c3dc03624
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2026-06-07 17:04:29 +0100

    feat: [AURA-393] make fireballs move

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBlast.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBlast.cpp
@@ -94,6 +94,7 @@ TArray<AAuraFireBall*> UAuraFireBlast::SpawnFireBalls()
 		);
 		
 		FireBall->DamageEffectParams = MakeDamageEffectParamsFromClassDefaults();
+		FireBall->ReturnToActor = GetAvatarActorFromActorInfo();
 		FireBalls.Add(FireBall);
 		FireBall->FinishSpawning(SpawnTransform);
 	}
